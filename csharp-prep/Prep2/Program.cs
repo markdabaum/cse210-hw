d39686e6583edefbc9ba913545c89cc1ee51b7ce
Mark Baumgarten <markdabaum@gmail.com>
2024-09-20 14:50:36 -0600

    Finished all of W01 Prove

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,66 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Prepping my variables
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        string letterGrade = "NA";
+        string gradeModifier = "";
+        string passedOrFailed = "";
+
+        // Defining the grade scale
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+            if (grade < 93)
+            {
+                gradeModifier = "-";
+            }
+        }
+        else if (grade < 60)
+        {
+            letterGrade = "F";
+        }
+        
+        else
+        {
+            if (grade >= 80)
+            {
+            letterGrade = "B";
+            }
+            else if (grade >= 70)
+            {
+                letterGrade = "C";
+            }
+            else if (grade >= 60)
+            {
+                letterGrade = "D";
+            }
+            
+
+            // Defining "+" or "-" modifiers
+            if (grade % 10 >= 3)
+            {
+                gradeModifier = "+";
+            }
+            else if (grade % 10 < 3)
+            {
+                gradeModifier = "-";
+            }
+        }
+
+        if (grade >= 70)
+        {
+            passedOrFailed = "passed";
+        }
+        else
+        {
+            passedOrFailed = "failed";
+        }
+        // Print
+        Console.WriteLine($"Your grade: {letterGrade}{gradeModifier}");
+        Console.WriteLine($"You {passedOrFailed}");
     }
 }
