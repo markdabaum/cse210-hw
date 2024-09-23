322b3c7118948bf6a1d20e5f1138bade3794f2c4
Mark Baumgarten <markdabaum@gmail.com>
2024-09-23 13:32:28 -0600

    Completed prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string again = "yes";
+        
+        while (again == "yes")
+        {
+            Random randomGenerator = new Random();
+            int num = randomGenerator.Next(0, 101);
+            // int num = 40;
+            int guess;
+            int guessCount = 0;
+
+            Console.WriteLine ("\nGuess the number!");
+            do
+            {
+                Console.Write("Enter your guess: ");
+                string input = Console.ReadLine();
+                guess = int.Parse(input);
+
+                if (num > guess)
+                {
+                    Console.WriteLine("Too low!");
+                }
+                else if (num < guess)
+                {
+                    Console.WriteLine("Too high");
+                }
+
+                guessCount++;
+
+            } while (guess != num);
+            
+            string plural;
+
+            if (guessCount == 1)
+            {
+                plural = "y";
+            }
+            else
+            {
+                plural = "ies";
+            }
+            Console.WriteLine($"You got it in {guessCount} tr{plural}!\n");
+
+            Console.Write("Play again (Yes/No)? ");
+            again = Console.ReadLine();
+        }
+        Console.WriteLine("Goodbye");
     }
 }
