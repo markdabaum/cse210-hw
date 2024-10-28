ba7cc244f767fcaaabbd553cbf4cc7837205fe2c
Mark Baumgarten <markdabaum@gmail.com>
2024-10-28 12:24:27 -0600

    Finished prepare material

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,17 @@
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string name, string topic)
+    {
+        _studentName = name;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        string summary = $"{_studentName} - {_topic}";
+        return summary;
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        WritingAssignment assignment = new("Samuel Bennet", "Multiplication", "The Causes of World War II by Mary Waters");
+        string summary = $"{assignment.GetSummary()}\n{assignment.GetTitle()}";
+        Console.WriteLine($"{summary}");
     }
 }
